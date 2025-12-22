3761eca081d9124a92a695cd91443fbc1d2ed664
sarialMa <rinnkai.ba2019@gmail.com>
2025-12-22 16:00:16 -0500

    first run of benchmark models

---
+++ apply_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+  
+
+#SBATCH --job-name=0shot_pv
+#SBATCH --mail-type=ALL
+#SBATCH --time=00-10:00:00
+#SBATCH --nodes=1
+#SBATCH --gpus=h200:2
+#SBATCH --mem=256G
+#SBATCH --partition=gpu_h200
+#SBATCH --output=%j_gpu_job.txt
+#SBATCH --mail-user=linhai.ma@yale.edu
+
+module load miniconda
+conda activate finben_vllm3
+cd /home/lm2445/project_pi_sjf37/lm2445/finben/FinBen
+sh pv_benchmark_initial_run.sh
--- pv_benchmark_initial_run.sh
+++ pv_benchmark_initial_run.sh
@@ -1,51 +1,55 @@
 #!/bin/bash
 
 lm_eval --model vllm \
-  --model_args "pretrained=meta-llama/Llama-3.1-8B-Instruct,tensor_parallel_size=1,gpu_memory_utilization=0.90,max_model_len=8192" \
+  --model_args "pretrained=meta-llama/Llama-3.3-70B-Instruct,tensor_parallel_size=2,gpu_memory_utilization=0.90,max_model_len=16384" \
   --tasks PvExtraction_full \
-  --num_fewshot 0 \
-  --batch_size 1 \
-  --output_path results/PV_vllm \
+  --num_fewshot 1 \
+  --batch_size auto \
+  --output_path results/PV_1222 \
   --log_samples \
   --apply_chat_template \
   --include_path ./tasks/pv_miner
 
-lm_eval --model hf \
-  --model_args "pretrained=Qwen/Qwen2.5-1.5B-Instruct" \
+lm_eval --model vllm \
+  --model_args "pretrained=Qwen/QwQ-32B-AWQ,tensor_parallel_size=2,gpu_memory_utilization=0.90,max_model_len=16384" \
   --tasks PvExtraction_full \
-  --num_fewshot 0 \
-  --device cuda:0 \
-  --batch_size 1 \
-  --output_path results/PV_initial\
+  --num_fewshot 1 \
+  --batch_size auto \
+  --output_path results/PV_1222 \
   --log_samples \
   --apply_chat_template \
   --include_path ./tasks/pv_miner
 
-# Run the Hugging Face LLM evaluation command
-# lm_eval --model hf \
-#         --model_args "pretrained=meta-llama/Llama-3.1-8B-Instruct" \
-#         --tasks PvExtraction_full \
-#         --num_fewshot 0 \
-#         --device cuda:0 \
-#         --batch_size auto \
-#         --output_path results/PV_initial \
-#         --log_samples \
-#         --apply_chat_template \
-#         --include_path ./tasks/pv_miner
+lm_eval --model vllm \
+  --model_args "pretrained=meta-llama/Llama-3.1-8B-Instruct,tensor_parallel_size=2,gpu_memory_utilization=0.90,max_model_len=16384" \
+  --tasks PvExtraction_full \
+  --num_fewshot 1 \
+  --batch_size auto \
+  --output_path results/PV_1222 \
+  --log_samples \
+  --apply_chat_template \
+  --include_path ./tasks/pv_miner
 
-        
+lm_eval --model vllm \
+  --model_args "pretrained=meta-llama/Llama-3.2-3B-Instruct,tensor_parallel_size=2,gpu_memory_utilization=0.90,max_model_len=16384" \
+  --tasks PvExtraction_full \
+  --num_fewshot 1 \
+  --batch_size auto \
+  --output_path results/PV_1222 \
+  --log_samples \
+  --apply_chat_template \
+  --include_path ./tasks/pv_miner
 
-# Run the Hugging Face VLLM evaluation command
-# lm_eval --model vllm \
-#         --model_args "pretrained=TheFinAI/FinLLaMA,tensor_parallel_size=4,gpu_memory_utilization=0.8,max_model_len=1024" \
-#         --tasks regAbbreviation \
-#         --num_fewshot 0 \
-#         --batch_size auto \
-#         --output_path results/abbr \
-#         --hf_hub_log_args "hub_results_org=TheFinAI,details_repo_name=lm-eval-llama-regulationAbbr-0shot-result,push_results_to_hub=True,push_samples_to_hub=True,public_repo=False" \
-#         --log_samples \
-#         --include_path ./tasks
-#         #--apply_chat_template \
+lm_eval --model vllm \
+  --model_args "pretrained=Qwen/Qwen2.5-1.5B-Instruct,tensor_parallel_size=2,gpu_memory_utilization=0.90,max_model_len=16384" \
+  --tasks PvExtraction_full \
+  --num_fewshot 1 \
+  --batch_size auto \
+  --output_path results/PV_1222 \
+  --log_samples \
+  --apply_chat_template \
+  --include_path ./tasks/pv_miner
 
-# output message
-#echo "Evaluation completed successfully!"
+
+
+
